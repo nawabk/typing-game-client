514f7a287d596ca94bb69747ae2bdae37620b74a
Md Khalid Nawab <mdkhalidnawab@Mds-MacBook-Pro.local>
2023-05-18 11:29:57 +0530

    added server url

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
-const URL = "http://192.168.0.132:5000";
+const URL =
+  process.env.NODE_ENV === "development"
+    ? "http://192.168.0.132:5000"
+    : "https://typing-fight.onrender.com/";
 
 export const socket = io(URL, {
   autoConnect: false,
